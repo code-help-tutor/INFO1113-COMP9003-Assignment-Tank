5ebf08144fdc0bba911c6bcfa7f17577af9c3125
Code Help Tutor <1823890830@qq.com>
2024-05-06 08:32:52 +0000

    WeChat cstutorcs INFO1113 COMP9003 Assignment Tank

---
+++ src/test/java/Tanks/SampleTest.java
@@ -0,0 +1,19 @@
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+package Tanks;
+
+
+import processing.core.PApplet;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SampleTest {
+
+    @Test
+    public void simpleTest() {
+        
+    }
+}
+
+//gradle test jacocoTestReport
